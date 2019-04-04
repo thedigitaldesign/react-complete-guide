e0964a9ac8e9c925953d3d01f431cca5f6b52c8e
Joshua Christensen <joshua@thedigitaldesign.com>
2019-04-03 23:54:16 -0600

    no message

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Persons extends Component {
+export default class Cockpit extends Component {
     render() {
         return ('')
     }
